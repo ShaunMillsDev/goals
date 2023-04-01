de4b30f82d0058909a0ce49909b52fffadf34974
Shaun Mills <shaun.cw.mills@gmail.com>
2023-04-01 02:24:57 +0100

    Added auth protection to all goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const { getGoals, setGoal, deleteGoal, updateGoal } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
 router.route('/')
-    .get(getGoals)
-    .post(setGoal)
+    .get(protect, getGoals)
+    .post(protect, setGoal)
 
 router.route('/:id')
-    .put(updateGoal)
-    .delete(deleteGoal)
+    .put(protect, updateGoal)
+    .delete(protect, deleteGoal)
 
 // export our router
 module.exports = router
